7da07344d626aa85ee22b46bcad6260783136222
worldwids81-sudo <worldwids81@gmail.com>
2025-08-22 23:50:19 +0530

    Update season1.js

--- naruto/season1.js
+++ naruto/season1.js
@@ -1,1 +1,20 @@
+export default function Season1() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Naruto Shippuden - Season 1</h1>
+      <ul className="space-y-2">
+        <li>
+          <a href="https://example.com/episode1" target="_blank" className="text-blue-600 underline">
+            Episode 1 - Enter Naruto!
+          </a>
+        </li>
+        <li>
+          <a href="https://example.com/episode2" target="_blank" className="text-blue-600 underline">
+            Episode 2 - Training Begins
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
